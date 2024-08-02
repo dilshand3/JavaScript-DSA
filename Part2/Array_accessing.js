acaec2d6590a94bebd9a10e2058fd526212d002e
dilshan.d3 <dilshan72404@gmail.com>
2024-08-02 16:24:09 +0530

    part2 completed

---
+++ Part2/Array_accessing.js
@@ -0,0 +1,5 @@
+const data = [2,32,3,53,24,245,33,13];
+const x = 3
+console.log(data[x])
+
+//accessing array using index
---
+++ Part2/Array_traversing.js
@@ -0,0 +1,4 @@
+const data = [2,32,3,53,24,245,33,13];
+for(let i = 0; i<data.length;i++){
+    console.log(`Array ${i} : ${data[i]}`)
+}
