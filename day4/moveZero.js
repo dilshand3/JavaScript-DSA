394e3403be7b8d5838b09576226f41f26f4e9a6c
dilshan.d3 <dilshan72404@gmail.com>
2025-10-18 20:57:40 +0530

    Add moveZero, pelindrome, and twoSum functions for array manipulation and palindrome checking

---
+++ day4/moveZero.js
@@ -0,0 +1,12 @@
+function moveZero(nums) {
+    let j = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            [nums[i], nums[j]] = [nums[j], nums[i]]
+            j++
+        }
+    }
+    return nums;
+}
+
+console.log(moveZero([1,0,2,0,3]));
---
+++ day4/pelindrome.js
@@ -0,0 +1,7 @@
+function pelindrome(str){
+    let i = 0;
+    let j = str.length - 1;
+    while(i<j){
+        
+    }
+}
